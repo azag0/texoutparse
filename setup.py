7bb8d36d91e1736dbe2c31d19ddefbe38aeddbee
Jan Hermann <dev@hermann.in>
2017-06-14 17:30:02 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='texoutparse',
+    version='0.1',
+    author='Jan Hermann',
+    author_email='dev@hermann.in',
+    url='https://github.com/azag0/texoutparse',
+    scripts=['texoutparse.py'],
+    license='Mozilla Public License 2.0',
+)
---
+++ texoutparse.py
@@ -0,0 +1,51 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import os
+import re
+
+
+def chunks(lst, n, fill=None):
+    for i in range(0, len(lst), n):
+        chunk = lst[i:i+n]
+        if len(chunk) != n:
+            chunk = chunk + (n-len(chunk))*[fill]
+        yield chunk
+
+
+def parse_word(word, stack):
+    m = re.match(r'(\(*)([^()]*)(\)*)$', word)
+    if not m:
+        return
+    paren_open, filename, paren_close = m.groups()
+    nopen = len(paren_open)
+    nclose = len(paren_close)
+    if (nopen or nclose) and (filename is '' or os.path.isfile(filename)):
+        filename = word[1:len(word)-nclose]
+        if nopen:
+            stack.append(filename)
+        if nclose:
+            for _ in range(nclose):
+                filename = stack.pop()
+        return True
+
+
+def run(fin, fout):
+    stack = ['ROOT']
+    for line in fin:
+        words = chunks(re.split(r'(\s+)', line), 2, '')
+        linebuff = ''
+        for word, sep in words:
+            if word is '' and sep is '':
+                break
+            if not parse_word(word, stack):
+                linebuff += word + sep
+        if not linebuff.endswith('\n'):
+            linebuff += '\n'
+        fout.write(stack[-1] + ':' + linebuff)
+    assert stack == ['ROOT']
+
+
+if __name__ == '__main__':
+    import sys
+    run(sys.stdin, sys.stdout)
